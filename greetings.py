f19aa6672b260101a9f690f5edc69d1ede16782a
Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2023-10-30 15:30:53 +0000

    Update greetings.py

--- greetings.py
+++ greetings.py
@@ -4,11 +4,11 @@ import time
 current_time = time.localtime()
 current_hour = current_time.tm_hour
 
-# Determine the appropriate greeting based on the given time
+# Determine the appropriate greeting based on the time
 if 5 <= current_hour < 12:
     greeting = "Good Morning"
 elif 12 <= current_hour < 17:
-    greeting = "Good Afternoon Sir"
+    greeting = "Good Afternoon"
 else:
     greeting = "Good Evening"
 
